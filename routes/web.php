eab6b174be7c9dd2937ba85692538d7e5c96540f
maki <maki080218@gmail.com>
2023-05-12 14:45:39 +0000

    mynews

---
+++ routes/web.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+use App\Http\Controllers\Admin\NewsController;
+Route::controller(NewsController::class)->prefix('admin')->group(function() {
+    Route::get('news/create', 'add');
+});
+
+
+//php Laravel09 課題3
+
+// Route::controller(AAAController::class)->group(function() {
+//     Route::get('xxx', 'bbb');
+// });
+
+//php Laravel09 課題4
+use App\Http\Controllers\Admin\ProfileController;
+Route::controller(ProfileController::class)->group(function() {
+    Route::get('admin/profile/create', 'add');
+    Route::get('admin/profile/edit', 'edit');
+});
+
+
+
